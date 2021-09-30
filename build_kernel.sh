165acc59dcd7f91d6a0341ddb2f6cf1e1cd6b952
Rama Bondan Prakoso <ramanarubp@gmail.com>
2021-09-30 11:02:20 +0000

    kernel: Add build scripts
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

---
+++ build_kernel.sh
@@ -0,0 +1,187 @@
+#!/bin/bash
+# simple build scripts for compiling kernel on this repo
+# note:
+# change telegram CHATID to yours
+# change OUTDIR if needed, by default its using /root directory
+
+##------------------------------------------------------##
+
+Help()
+{
+  echo "Usage: [--help|-h|-?] [--clone|-c] [--lto]"
+  echo "$0 <defconfig> <token> [Other Args]"
+  echo -e "\t--clone: Clone compiler"
+  echo -e "\t--lto: Enable Clang LTO"
+  echo -e "\t--help: To show this info"
+}
+
+##------------------------------------------------------##
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+  --clone|-c)
+  CLONE=true
+  shift
+  ;;
+  --lto)
+  LTO=true
+  shift
+  ;;
+  --help|-h|-?)
+  Help
+  exit
+  ;;
+  *)
+  POSITIONAL+=("$1")
+  shift
+  ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [[ ! -n $2 ]]; then
+  echo "ERROR: Enter all needed parameters"
+  usage
+  exit
+fi
+
+CONFIG=$1
+TOKEN=$2
+
+echo "This is your setup config"
+echo
+echo "Using defconfig: ""$CONFIG""_defconfig"
+echo "Clone dependencies: $([[ ! -z "$CLONE" ]] && echo "true" || echo "false")"
+echo "Enable LTO Clang: $([[ ! -z "$LTO" ]] && echo "true" || echo "false")"
+echo
+read -p "Are you sure? " -n 1 -r
+! [[ $REPLY =~ ^[Yy]$ ]] && exit
+echo
+
+##------------------------------------------------------##
+
+tg_post_msg() {
+  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$CHATID" \
+       -d "disable_web_page_preview=true" \
+       -d "parse_mode=html" \
+       -d text="$1"
+}
+
+##----------------------------------------------------------------##
+
+tg_post_build() {
+  curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+                      -F chat_id="$CHATID"  \
+                      -F "disable_web_page_preview=true" \
+                      -F "parse_mode=html" \
+                      -F caption="$2"
+}
+##----------------------------------------------------------------##
+
+repack() {
+  cd "$KERNEL_DIR/ramdisk" || exit 1
+  rm *.gz-dtb
+  cp "$OUTDIR"/arch/arm64/boot/Image.gz-dtb .
+  bash bootimg.sh
+  mv *.img "$KNAME"-"${DATE}".img
+  cd - || exit
+}
+
+##----------------------------------------------------------------##
+
+build_kernel() {
+  [[ $LTO == true ]] && echo "CONFIG_LTO_CLANG=y" >> arch/arm64/configs/"$DEFCONFIG"
+  echo "-Genom-R-$CONFIG" > localversion
+  make O="$OUTDIR" ARCH=arm64 "$DEFCONFIG"
+  make -j"$PROCS" O="$OUTDIR" \
+                  ARCH=arm64 \
+                  CC=clang \
+                  CLANG_TRIPLE=aarch64-linux-gnu- \
+                  CROSS_COMPILE=aarch64-linux-android- \
+                  CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                  LD=ld.lld \
+                  NM=llvm-nm \
+                  OBJCOPY=llvm-objcopy
+}
+
+##----------------------------------------------------------------##
+
+export OUTDIR=/root
+
+if [[ $CLONE == true ]]
+then
+  echo "Cloning dependencies"
+  mkdir "$OUTDIR"/clang-llvm
+  mkdir "$OUTDIR"/gcc64-aosp
+  mkdir "$OUTDIR"/gcc32-aosp
+  ! [[ -f "$OUTDIR"/clang-r383902.tar.gz ]] && wget https://android.googlesource.com/platform//prebuilts/clang/host/linux-x86/+archive/3857008389202edac32d57008bb8c99d2c957f9d/clang-r383902.tar.gz -P "$OUTDIR"
+  tar -C "$OUTDIR"/clang-llvm/ -zxvf "$OUTDIR"/clang-r383902.tar.gz
+  ! [[ -f "$OUTDIR"/android-11.0.0_r35.tar.gz ]] && wget https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+archive/refs/tags/android-11.0.0_r35.tar.gz -P "$OUTDIR"
+  tar -C "$OUTDIR"/gcc64-aosp/ -zxvf "$OUTDIR"/android-11.0.0_r35.tar.gz
+  ! [[ -f "$OUTDIR"/android-11.0.0_r34.tar.gz ]] && wget http://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/+archive/refs/tags/android-11.0.0_r34.tar.gz -P "$OUTDIR"
+  tar -C "$OUTDIR"/gcc32-aosp/ -zxvf "$OUTDIR"/android-11.0.0_r34.tar.gz
+  git clone https://github.com/rama982/AnyKernel3 --depth=1 "$OUTDIR"/AnyKernel
+fi
+
+#telegram env
+CHATID=-1001459070028
+BOT_MSG_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+BOT_BUILD_URL="https://api.telegram.org/bot$TOKEN/sendDocument"
+
+# env
+export DEFCONFIG=$CONFIG"_defconfig"
+export TZ="Asia/Jakarta"
+export KERNEL_DIR=$(pwd)
+export KNAME="Genom-R-$CONFIG-BETA"
+export IMAGE="${OUTDIR}/arch/arm64/boot/Image.gz-dtb"
+export DATE=$(date "+%Y%m%d-%H%M")
+export BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+export PATH="${OUTDIR}/clang-llvm/bin:${OUTDIR}/gcc64-aosp/bin:${OUTDIR}/gcc32-aosp/bin:${PATH}"
+export KBUILD_COMPILER_STRING="$(${OUTDIR}/clang-llvm/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
+export KBUILD_BUILD_USER=rama982
+export COMMIT_HEAD=$(git log --oneline -1)
+export PROCS=$(nproc --all)
+export DISTRO=$(cat /etc/issue)
+export KERVER=$(make kernelversion)
+
+# start build
+tg_post_msg "
+Build is started
+<b>OS: </b>$DISTRO
+<b>Date : </b>$(date)
+<b>Device : </b>$CONFIG
+<b>Core Count : </b>$PROCS cores
+<b>Branch : </b>$BRANCH
+<b>Top Commit : </b>$COMMIT_HEAD
+"
+
+BUILD_START=$(date +"%s")
+
+build_kernel
+
+BUILD_END=$(date +"%s")
+DIFF=$((BUILD_END - BUILD_START))
+
+if [[ -f $IMAGE ]]
+then
+  repack
+  BOOTIMG=$(ls "$KERNEL_DIR"/ramdisk/*.img)
+  tg_post_build "$BOOTIMG" "
+<b>Build took : </b>$((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)
+<b>Kernel Version : </b>$KERVER
+<b>Compiler: </b>$(grep LINUX_COMPILER ${OUTDIR}/include/generated/compile.h  |  sed -e 's/.*LINUX_COMPILER "//' -e 's/"$//')
+<b>Enable LTO Clang: </b>$([[ ! -z "$LTO" ]] && echo "true" || echo "false")
+"
+else
+  tg_post_msg "<b>Build took : </b>$((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) but error"
+fi
+
+# reset git
+git reset --hard HEAD
+
+##----------------*****-----------------------------##
---
+++ ramdisk/bootimg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Original script by arter97 <Park Ju Hyung <qkrwngud825@gmail.com>
+# Modified
+
+export RAMDISKDIR=`readlink -f .`
+export PARTITION_SIZE=67108864
+
+export PATH="$RAMDISKDIR/bin/:$PATH"
+
+export OS="11.0.0"
+export SPL="2021-06"
+
+echo "ramdiskdir = $RAMDISKDIR"
+
+echo "Making new boot image"
+mkbootimg \
+    --kernel $RAMDISKDIR/Image.gz-dtb \
+    --ramdisk $RAMDISKDIR/ramdisk.cpio.gz \
+    --cmdline 'bootopt=64S3,32N2,64N2' \
+    --base           0x40078000 \
+    --pagesize       2048 \
+    --kernel_offset  0x00008000 \
+    --ramdisk_offset 0x07c08000 \
+    --second_offset  0xbff88000 \
+    --tags_offset    0x0bc08000 \
+    --dtb            ramdisk.dtb \
+    --dtb_offset     0x0bc08000 \
+    --os_version     $OS \
+    --os_patch_level $SPL \
+    --header_version 2 \
+    -o $RAMDISKDIR/boot.img
+
+GENERATED_SIZE=$(stat -c %s boot.img)
+if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
+	echo "boot.img size larger than partition size!" 1>&2
+	exit 1
+fi
+
+echo "done"
+ls -al boot.img
+echo ""
